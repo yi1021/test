18a3f906c487961284755278b05b384d8f9ed8e8
yi1021 <b44g17@gmail.com>
2015-08-19 21:31:10 +0900

    test

---
+++ schedule_list.php
@@ -0,0 +1,9 @@
+﻿<?php 
+$filename = "C:\\xampp\htdocs\schedule\sample.txt"; 
+$schedule_list = file($filename); 
+
+foreach ($schedule_list as $line) { 
+    list($schedule_date, $title, $body) = explode("|", $line); 
+    print("日付：$schedule_date タイトル：$title 内容：$body <br>"); 
+} 
+?>
